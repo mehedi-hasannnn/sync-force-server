b0765cac2bfd836c23824762b28f2732a5df51c8
mehedi-hasannnn <mhasan2999@gmail.com>
2025-05-24 14:04:38 +0600

    json web token and stripe setup done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require("jsonwebtoken");
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
