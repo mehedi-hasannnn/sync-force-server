6fde82e5a3ddc807e2909cf5b9f191da594aab0a
mehedi-hasannnn <mhasan2999@gmail.com>
2025-05-24 18:13:04 +0600

    all payment fetching api implemented

--- index.js
+++ index.js
@@ -307,6 +307,14 @@ async function run() {
     });
 
 
+    // get all payments
+
+    app.get("/payments", verifyToken, async (req, res) => {
+      const result = await paymentCollection.find().toArray();
+      res.send(result);
+    });
+
+
 
 
     // Connect the client to the server	(optional starting in v4.7)
